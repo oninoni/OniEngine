71b10749f3ce90d08ac3703b1adbffa4ac049890
Jan Ziegler <jan@cheap-purple.com>
2016-11-28 14:54:12 +0100

    Forgot this file XD

---
+++ Engine/Engine/TextureArray.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+
+#include "RecourceLoader.h"
+
+#include "TextureArray.h"
+
+TextureArray::TextureArray(uint layers, uint width, uint height, GLenum format) {
+    this->width = width;
+    this->height = height;
+    this->layers = layers;
+
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D_ARRAY, textureID);
+
+    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    glTexStorage3D(GL_TEXTURE_2D_ARRAY, 1, GL_RGBA, width, height, layers);
+    //glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, format, width, height, layers, GL_FALSE, NULL, GL_UNSIGNED_BYTE, 0);
+}
+
+TextureArray::~TextureArray() {
+    glDeleteTextures(1, &textureID);
+}
+
+void TextureArray::loadImage(uint layer, string fileName) {
+    Image image = RecourceLoader::loadTexture(fileName);
+
+    if (width == 0 || height == 0 || this->width != image.width || this->height != image.height) {
+        cerr << "Image sizes in 1 Texture do not fit! " << endl;
+        assert(false);
+    }
+
+    glBindTexture(GL_TEXTURE_2D_ARRAY, textureID);
+    glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0, 0, 0, layer, width, height, 1, GL_RGBA, GL_UNSIGNED_BYTE, image.data.data());
+}
+
+void TextureArray::bind(uint unit) {
+    assert(unit >= 0 && unit < 32);
+
+    glActiveTexture(GL_TEXTURE0 + unit);
+    glBindTexture(GL_TEXTURE_2D_ARRAY, textureID);
+}
